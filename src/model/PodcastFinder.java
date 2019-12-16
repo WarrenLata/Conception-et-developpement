018319f07b6c935dac72044b2976c5250278df25
LATA Warren <warren.lata@telecomnancy.eu>
2019-12-17 00:04:25 +0100

    Upload New File

---
+++ src/model/PodcastFinder.java
@@ -0,0 +1,61 @@
+package Model;
+
+
+
+
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
+import org.jdom2.Element;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
+
+
+
+public class PodcastFinder {
+    private URL targetURL;
+    private SyndFeed feed;
+
+    public PodcastFinder (String rssURL) {
+        try {
+            targetURL = new URL(rssURL);
+            SyndFeedInput input = new SyndFeedInput();
+
+            try {
+                feed = input.build(new XmlReader(targetURL));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (com.rometools.rome.io.FeedException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getTitle () {
+        return feed.getTitle();
+    }
+
+    public String getAuthor () {
+        return feed.getAuthor();
+    }
+
+    public String getForeignElementValue (String el) {
+        for (Element element : feed.getForeignMarkup()) {
+            if (element.getName() == el) {
+                return element.getText();
+            }
+        }
+
+        return "N/A";
+    }
+
+    public List<SyndEntry> getEntries () {
+        return feed.getEntries();
+    }
+}
