d5cffd1a13b3d1f864544e7cce3a48582ee1e1d7
HMIZA Jaber <jaber.hmiza.1@telecomnancy.eu>
2019-12-18 22:03:42 +0100

    Update Main.java

--- Fortify/src/main/java/sample/Main.java
+++ Fortify/src/main/java/sample/Main.java
@@ -1,6 +1,9 @@
 package sample;
 
+import javafx.geometry.Insets;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import model.Episode;
 import model.Podcast;
 import model.PodcastFinder;
@@ -15,30 +18,33 @@ import model.*;
 import sample.EpisodeView;
 import sample.SearchView;
 
+import java.io.File;
+
 public class Main extends Application {
 
 
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        String current=new java.io.File(".").getCanonicalPath();
+        String current=new File(".").getCanonicalPath();
         System.out.println(current+ " PATH! ");
         PodcastApp pd = new PodcastApp("./src/main/resources");
         System.out.println("La taille de json : " + pd.getData().size());
         System.out.println("la catégorie est : " + pd.getData().get(0).getTitle());
         System.out.println("la catégorie est : " + pd.getData().get(1).getTitle());
-        BorderPane global_layout = new BorderPane();
+        VBox global_layout = new VBox();
         PodcastsView centre_page = new PodcastsView(pd);
+        GridPane main_part = new GridPane();
         VBox favoris = new VBox();
 
         //String title, int nombreEpisodes,ArrayList<Episode> episodes, String description
         // String url,String title,String image,String description
         SearchView searchBar = new SearchView();
-        global_layout.setTop(searchBar);
-        primaryStage.setTitle("Podhub");
+        global_layout.getChildren().add(searchBar);
+        primaryStage.setTitle("FORTIFY");
         primaryStage.setScene(new Scene(global_layout, 1280, 800));
         //global_layout.getChildren().add(searchBar);
-        global_layout.setCenter(centre_page);
+        //global_layout.setCenter(centre_page);
 
 
 
@@ -69,7 +75,26 @@ public class Main extends Application {
         VideoPlayerController video = new VideoPlayerController(pd.getPodcastChoisi().getEpisodeList().get(0).getURL());
         //PodcastsView list_podcast=new PodcastsView("http://sw7x7.libsyn.com/rss",video);
         EpisodeView episode_part = new EpisodeView(pd,video);
-        global_layout.setRight(episode_part);
+        //global_layout.setRight(episode_part);
+        //global_layout.setStyle("-fx-background-color: #000000; -fx-pref-height: 800; -fx-pref-width: 1000");
+        centre_page.setStyle("-fx-pref-height: 500;" +
+                "    -fx-pref-width: 900;" +
+                "    -fx-background-color: rgb(46, 46, 46);" +
+                "    -fx-background-radius: 15px;");
+        centre_page.setPadding(new Insets(20));
+        episode_part.setStyle("-fx-pref-height: 500;" +
+                "    -fx-pref-width: 374;"
+                +"-fx-background-color: rgb(46, 46, 46);-fx-background-radius: 15px; ");
+        episode_part.setPadding(new Insets(20));
+        searchBar.setStyle("-fx-background-color: #484848;" +
+                "    -fx-pref-height: 62;" +
+                "    -fx-pref-width: 1000;");
+        main_part.addRow(0,centre_page,episode_part);
+        main_part.setPadding(new Insets(20));
+        main_part.setStyle("    -fx-pref-height: 500;" +
+                "    -fx-pref-width: 1000;");
+        global_layout.getChildren().add(main_part);
+        main_part.setHgap(10);
         /*
         centre_page.getChildren().add(favoris);
         centre_page.getChildren().add(list_podcast);
@@ -92,7 +117,7 @@ public class Main extends Application {
         Parent player = FXMLLoader.load(getClass().getResource("sample.fxml"));
         //  primaryStage.setTitle("Hello World");
         //primaryStage.setScene(new Scene(root));
-        global_layout.setBottom(player);
+        global_layout.getChildren().add(player);
 
         primaryStage.show();
     }
