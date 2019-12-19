383ce633f448367811d6948b36f6be6c39832bc8
LATA Warren <warren.lata@telecomnancy.eu>
2019-12-19 22:38:48 +0100

    Update Podcast.java

--- Fortify/src/main/java/model/Podcast.java
+++ Fortify/src/main/java/model/Podcast.java
@@ -83,11 +83,12 @@ public class Podcast implements RSSItem{
     public void loadFeed(String URL) throws Exception {
         feed = new PodcastFinder(URL);
         title = feed.getTitle();
+        System.out.println("feed title"+feed.getTitle());
         author = feed.getForeignElementValue("author");
-        episodes = getEntriesTitles();
         imageURL=feed.getImageUrl();
         description=feed.getDescription();
         language=feed.getLangage();
+        episodes = getEntriesTitles();
 
     }
 
@@ -99,35 +100,40 @@ public class Podcast implements RSSItem{
 
         for (final Iterator iter = entries.iterator(); iter.hasNext(); ) {
             final SyndEntry entry = (SyndEntry) iter.next();
-            Episode episodeListEntry = new Episode();
+            Episode episodeListEntry = new Episode(); // construction d'une episode
+
             episodeListEntry.setName(entry.getTitle());
             episodeListEntry.setDescription(entry.getDescription().getValue());
-
-            //   final Element foreign = (Element) entry.getForeignMarkup().get(3);
-            //System.out.println(foreign.getText());
-
-//      for (final Iterator foreignIterator = entry.getForeignMarkup().iterator(); foreignIterator.hasNext(); ) {
-//        final Element foreign = (Element) foreignIterator.next();
-//        System.out.println(foreign.get);
-//      }
-       /*
-            for (final Iterator enclosureIterator = entry.getEnclosures().iterator(); enclosureIterator.hasNext(); ) {
-                final SyndEnclosureImpl enclosure = (SyndEnclosureImpl) enclosureIterator.next();
-                episodeListEntry.setType(enclosure.getType());
-                episodeListEntry.setURL(enclosure.getUrl());
+            if(entry.getEnclosures().get(0) != null){
+                episodeListEntry.setURL(entry.getEnclosures().get(0).getUrl());
+                episodeListEntry.setType(entry.getEnclosures().get(0).getType());
             }
-*/
-            /*for (final Iterator moduleIter = entry.getModules().iterator(); moduleIter.hasNext(); ) {
-                final DCModuleImpl module = (DCModuleImpl) moduleIter.next();
-                SimpleDateFormat originalDateFormat = new SimpleDateFormat("EE MMM dd hh:mm:ss zz yyyy");
-                Date formattedDate = originalDateFormat.parse(module.getDate().toString());
-                SimpleDateFormat displayDateFormatter = new SimpleDateFormat("yyyy-MM-dd");
-                episodeListEntry.setDate(displayDateFormatter.format(formattedDate));
-            }*/
 
-            episodeListEntryList.add(episodeListEntry);
+           // episodeListEntry.setURL(entry.getEnclosures().get(1).getUrl());
+
+            episodeListEntry.setDate(entry.getPublishedDate().toString());
+         //   episodeListEntry.setType(entry.getEnclosures().get(0).get);
+
+
+            if ((episodeListEntry != null) && (episodeListEntry.getURL() != null) && (episodeListEntry.getName() != null)) {
+                  System.out.println("il et là : desription  : " + episodeListEntry.getDescription());
+                  System.out.println("il et là : URL 2 : " + entry.getEnclosures().get(0).getUrl());
+                 // System.out.println("il et là : URL 2 : " + entry.getEnclosures().get(1).getUrl());
+
+                  System.out.println("il et là : Le titre de l'épisode :" + episodeListEntry.getName());
+                  System.out.println("il et là : La date de l'épisode " + episodeListEntry.getDate());
+
+                  episodeListEntryList.add(episodeListEntry);
+
+
+
+            } else {
+                continue;
+            }
+
+
+
         }
-
         return episodeListEntryList;
 
     }
