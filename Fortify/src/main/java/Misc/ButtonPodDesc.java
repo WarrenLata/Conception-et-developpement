ecc2e9e3f79f426f539968d9329f18c7bd7399d6
HMIZA Jaber <jaber.hmiza.1@telecomnancy.eu>
2019-12-19 22:15:06 +0100

    Update ButtonPodDesc.java

--- Fortify/src/main/java/Misc/ButtonPodDesc.java
+++ Fortify/src/main/java/Misc/ButtonPodDesc.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import model.Podcast;
 import model.PodcastApp;
 import model.PodcastProperties;
+import sample.EpisodeView;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,11 +15,12 @@ public class ButtonPodDesc extends Button  {
     private PodcastApp app;
     public PodcastProperties podcastliee;
 
-    public ButtonPodDesc(String desc,PodcastApp application,PodcastProperties podcastliee){
+    public ButtonPodDesc(String desc, PodcastApp application, PodcastProperties podcastliee){
         super(desc);
         this.app=application;
         this.podcastliee=podcastliee;
         this.setOnAction(new EcouteurPodDesc(application, podcastliee));
+
     }
 
 
