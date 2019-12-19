4b3b637ce5387b4b27d31086c4d530120884acf2
REHIOUI Walid <walid.rehioui@telecomnancy.eu>
2019-12-19 22:32:04 +0100

    Update PodcastListLoader.java

--- Fortify/src/main/java/model/PodcastListLoader.java
+++ Fortify/src/main/java/model/PodcastListLoader.java
@@ -58,10 +58,33 @@ public class PodcastListLoader {
         try {
             Gson gson = new Gson();
             gson = new GsonBuilder().setPrettyPrinting().create();
-            String json = gson.toJson(podcastEntry);
+            ArrayList<PodcastProperties> listeToappend = new ArrayList<PodcastProperties>();
+            int a  = podcastEntry.getProperties().size();
+            listeToappend.add(podcastEntry.getProperties().get(a-1));
+            PodcastListEntry podacstor = new PodcastListEntry(listeToappend);
+            String json = gson.toJson(podacstor);
+            System.out.println("this is the size that we're going to add Using the method updateLocalPOdcastData :"+ podcastEntry.getProperties().size());
+            FileWriter writer = new FileWriter(configPath + "/" + fileName,true);
+            writer.write(json);
+            writer.close();
+        //    for (int i=0; i<podcastEntry.getProperties().size();i++){
+         //       podcastEntry.removePodcast(i);
+         //    }
+        } catch (IOException e) {
+            System.out.println("Error writing to file '" + configPath + "/" + fileName);
+        }
+    }
+
+    public void ClearAndSave(String fileName, PodcastListEntry podcastentered){
+        try {
+            Gson gson = new Gson();
+            gson = new GsonBuilder().setPrettyPrinting().create();
+            String json = gson.toJson(podcastentered);
+            //      System.out.println("this is the size that we're going to add Using the method Clear and Save:"+ liste.getProperties().size());
             FileWriter writer = new FileWriter(configPath + "/" + fileName);
             writer.write(json);
             writer.close();
+            podcastentered.getProperties().clear();
         } catch (IOException e) {
             System.out.println("Error writing to file '" + configPath + "/" + fileName);
         }
